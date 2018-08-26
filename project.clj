076e5567bdf747ff655eded38e173c0b5775bf3d
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2018-08-26 09:56:00 +0200

    add lein-shell

--- project.clj
+++ project.clj
@@ -1,14 +1,16 @@
-(defproject lein-changelog "0.0.0"
+(defproject lein-changelog "0.1.0"
   :description "A Leiningen plugin to automate changelog tasks."
   :url "https://github.com/dryewo/lein-changelog"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies []
   :plugins [[lein-cloverage "1.0.9"]
+            [lein-shell "0.5.0"]
             ;[lein-changelog "0.0.0"]
             [lein-ancient "0.6.15"]]
   :eval-in-leiningen true
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}}
+  :deploy-repositories [["releases" :clojars]]
   :aliases {"update-readme-version" ["shell" "sed" "-i" "s/\\\\[lein-changelog \"[0-9.]*\"\\\\]/[lein-changelog \"${:version}\"]/" "README.md"]}
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version"]
